f8e1b698e425dbab56f6f407eac1aa55794ccc72
TechieTeee <techieteee@protonmail.com>
2024-06-30 00:41:00 +0000

    Updated BeBetterVeBetterIcon animation

--- apps/frontend/src/components/Icon/BeBetterVeBetterIcon.tsx
+++ apps/frontend/src/components/Icon/BeBetterVeBetterIcon.tsx
@@ -1,16 +1,36 @@
-import { Image, ImageProps, VStack } from "@chakra-ui/react"
-import React from "react"
+import { Image, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 type Props = {
-  beBetterProps?: ImageProps
-  veBetterProps?: ImageProps
-}
+  beBetterProps?: ImageProps;
+  veBetterProps?: ImageProps;
+};
 
 /**
  * BeBetterVeBetterIcon displays the BeBetterVeBetter logo
  */
-export const BeBetterVeBetterIcon: React.FC<Props> = ({ veBetterProps, beBetterProps }) => (
-  <VStack spacing={2} align="flex-start" w="full">
-    <Image src="/Foodi3_Logo.svg"  {...veBetterProps}  width="33%" height="auto"/>
-  </VStack>
-)
+export const BeBetterVeBetterIcon: React.FC<Props> = ({ veBetterProps, beBetterProps }) => {
+  const [hovered, setHovered] = useState(false);
+
+  const handleHover = () => {
+    setHovered(!hovered);
+  };
+
+  return (
+    <VStack spacing={2} align="flex-start" w="full">
+      <Image
+        src="/Foodi3_Logo.svg"
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHover}
+        {...veBetterProps}
+        width="33%"
+        height="auto"
+        transition="transform 0.3s ease-in-out"
+        transform={hovered ? "rotateY(180deg)" : "rotateY(0deg)"}
+        _hover={{
+          transform: "rotateY(180deg)",
+        }}
+      />
+    </VStack>
+  );
+};
